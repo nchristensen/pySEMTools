3bcd526523ada8aa6b95ce52c410437b417ff118
adperezm <adalberto0524@gmail.com>
2024-07-05 16:14:00 +0200

    Made ppymech pretty

--- pynektools/ppymech/neksuite.py
+++ pynektools/ppymech/neksuite.py
@@ -1,12 +1,24 @@
+""" Module that wraps the parallel IO calls and put data in the pymech format"""
+
+import sys
 from mpi4py import MPI
 import numpy as np
 from pymech.neksuite.field import read_header
 from pymech.core import HexaData
 from pymech.neksuite.field import Header
-from .parallel_io import *
+from .parallel_io import (
+    fld_file_read_vector_field,
+    fld_file_read_field,
+    fld_file_write_vector_field,
+    fld_file_write_field,
+    fld_file_write_vector_metadata,
+    fld_file_write_metadata,
+)
 
 
-class io_helper:
+class IoHelper:
+    """Class to contain general information of the file and some buffers"""
+
     def __init__(self, h):
 
         self.fld_data_size = h.wdsz
@@ -26,7 +38,26 @@ class io_helper:
         self.temp_variables = h.nb_vars[3]
         self.scalar_variables = h.nb_vars[4]
 
+        # Allocate optional variables
+        self.nelv = None
+        self.n = None
+        self.offset_el = None
+
+        self.m = None
+        self.pe_rank = None
+        self.pe_size = None
+        self.l = None
+        self.r = None
+        self.ip = None
+
+        self.tmp_sp_vector = None
+        self.tmp_dp_vector = None
+        self.tmp_sp_field = None
+        self.tmp_dp_field = None
+
     def element_mapping(self, comm):
+        """Maps the number of elements each processor has
+        in equally"""
         rank = comm.Get_rank()
         size = comm.Get_size()
 
@@ -36,15 +67,16 @@ class io_helper:
         self.offset_el = rank * self.nelv
 
     def element_mapping_load_balanced_linear(self, comm):
-
-        self.M = self.glb_nelv
+        """Maps the number of elements each processor has
+        in a linearly load balanced manner"""
+        self.m = self.glb_nelv
         self.pe_rank = comm.Get_rank()
         self.pe_size = comm.Get_size()
-        self.L = np.floor(np.double(self.M) / np.double(self.pe_size))
-        self.R = np.mod(self.M, self.pe_size)
-        self.Ip = np.floor(
+        self.l = np.floor(np.double(self.m) / np.double(self.pe_size))
+        self.r = np.mod(self.m, self.pe_size)
+        self.ip = np.floor(
             (
-                np.double(self.M)
+                np.double(self.m)
                 + np.double(self.pe_size)
                 - np.double(self.pe_rank)
                 - np.double(1)
@@ -52,13 +84,12 @@ class io_helper:
             / np.double(self.pe_size)
         )
 
-        self.nelv = int(self.Ip)
-        self.offset_el = int(self.pe_rank * self.L + min(self.pe_rank, self.R))
+        self.nelv = int(self.ip)
+        self.offset_el = int(self.pe_rank * self.l + min(self.pe_rank, self.r))
         self.n = self.lxyz * self.nelv
 
-        # print("Reading Rank: {}, nelv: {}, offset: {}".format(self.pe_rank, self.nelv, self.offset_el))
-
     def element_mapping_from_parallel_hexadata(self, comm):
+        """Find the element mapping when the input data was already parallel and divided equally"""
         rank = comm.Get_rank()
         size = comm.Get_size()
 
@@ -75,8 +106,8 @@ class io_helper:
         self.glb_nelv = self.nelv * size
 
     def element_mapping_from_parallel_hexadata_mpi(self, comm):
-        rank = comm.Get_rank()
-        size = comm.Get_size()
+        """Find the element mapping when the input data was already parallel and divided
+        in a linearly load balanced manner"""
 
         # io helper assume that the nel in header is the global one
         # So we have to correct if the header is initialized from a parallel hexadata object
@@ -95,9 +126,8 @@ class io_helper:
         comm.Allreduce(sendbuf, recvbuf)
         self.glb_nelv = recvbuf[0]
 
-        # print("Writing Rank: {}, nelv: {}, offset: {}, global_nelv: {}".format(rank, self.nelv, self.offset_el, self.glb_nelv))
-
     def allocate_temporal_arrays(self):
+        """'Allocate temporal arrays for reading and writing fields"""
         self.tmp_sp_vector = np.zeros(self.gdim * self.n, dtype=np.single)
         self.tmp_dp_vector = np.zeros(self.gdim * self.n, dtype=np.double)
         self.tmp_sp_field = np.zeros(self.n, dtype=np.single)
@@ -105,21 +135,18 @@ class io_helper:
 
 
 def preadnek(filename, comm, data_dtype="float64"):
+    """Read and fld file and return a pymech hexadata object (Parallel)"""
 
-    # Get mpi info
-    rank = comm.Get_rank()
-    size = comm.Get_size()
-
-    MPI_INT_SIZE = MPI.INT.Get_size()
-    MPI_REAL_SIZE = MPI.REAL.Get_size()
-    MPI_DOUBLE_SIZE = MPI.DOUBLE.Get_size()
-    MPI_CHARACTER_SIZE = MPI.CHARACTER.Get_size()
+    mpi_int_size = MPI.INT.Get_size()
+    mpi_real_size = MPI.REAL.Get_size()
+    # mpi_double_size = MPI.DOUBLE.Get_size()
+    mpi_character_size = MPI.CHARACTER.Get_size()
 
     # Read the header
     header = read_header(filename)
 
     # Initialize the io helper
-    ioh = io_helper(header)
+    ioh = IoHelper(header)
 
     # Find the appropiate partitioning of the file
     # ioh.element_mapping(comm)
@@ -141,17 +168,17 @@ def preadnek(filename, comm, data_dtype="float64"):
     fh = MPI.File.Open(comm, filename, MPI.MODE_RDONLY)
 
     # Read the test pattern
-    mpi_offset = 132 * MPI_CHARACTER_SIZE
+    mpi_offset = 132 * mpi_character_size
     test_pattern = np.zeros(1, dtype=np.single)
     fh.Read_at_all(mpi_offset, test_pattern, status=None)
 
     # Read the indices?
-    mpi_offset += MPI_REAL_SIZE
+    mpi_offset += mpi_real_size
     idx = np.zeros(ioh.nelv, dtype=np.intc)
-    byte_offset = mpi_offset + ioh.offset_el * MPI_INT_SIZE
+    byte_offset = mpi_offset + ioh.offset_el * mpi_int_size
     fh.Read_at_all(byte_offset, idx, status=None)
     data.elmap = idx
-    mpi_offset += ioh.glb_nelv * MPI_INT_SIZE
+    mpi_offset += ioh.glb_nelv * mpi_int_size
 
     # Read the coordinates
     if ioh.pos_variables > 0:
@@ -207,15 +234,12 @@ def preadnek(filename, comm, data_dtype="float64"):
 
 
 def pwritenek(filename, data, comm):
+    """write and fld file and from a pymech hexadata object (Parallel)"""
 
-    # Get mpi info
-    rank = comm.Get_rank()
-    size = comm.Get_size()
-
-    MPI_INT_SIZE = MPI.INT.Get_size()
-    MPI_REAL_SIZE = MPI.REAL.Get_size()
-    MPI_DOUBLE_SIZE = MPI.DOUBLE.Get_size()
-    MPI_CHARACTER_SIZE = MPI.CHARACTER.Get_size()
+    mpi_int_size = MPI.INT.Get_size()
+    mpi_real_size = MPI.REAL.Get_size()
+    # mpi_double_size = MPI.DOUBLE.Get_size()
+    mpi_character_size = MPI.CHARACTER.Get_size()
 
     # instance a dummy header
     dh = Header(
@@ -231,7 +255,7 @@ def pwritenek(filename, data, comm):
     )
 
     # instance the parallel io helper with the dummy header
-    ioh = io_helper(dh)
+    ioh = IoHelper(dh)
 
     # Get actual element mapping from the parallel hexadata
     # We need this since what we have in data.nel is the
@@ -262,21 +286,21 @@ def pwritenek(filename, data, comm):
     # Write the header
     mpi_offset = 0
     fh.Write_all(h.as_bytestring())
-    mpi_offset += 132 * MPI_CHARACTER_SIZE
+    mpi_offset += 132 * mpi_character_size
 
     # write test pattern
     test_pattern = np.zeros(1, dtype=np.single)
     test_pattern[0] = 6.54321
     fh.Write_all(test_pattern)
-    mpi_offset += MPI_REAL_SIZE
+    mpi_offset += mpi_real_size
 
     # write element mapping
     idx = np.zeros(ioh.nelv, dtype=np.intc)
     for i in range(0, data.nel):
         idx[i] = data.elmap[i]
-    byte_offset = mpi_offset + ioh.offset_el * MPI_INT_SIZE
+    byte_offset = mpi_offset + ioh.offset_el * mpi_int_size
     fh.Write_at_all(byte_offset, idx, status=None)
-    mpi_offset += ioh.glb_nelv * MPI_INT_SIZE
+    mpi_offset += ioh.glb_nelv * mpi_int_size
 
     # Write the coordinates
     if ioh.pos_variables > 0:
--- pynektools/ppymech/parallel_io.py
+++ pynektools/ppymech/parallel_io.py
@@ -1,9 +1,10 @@
-''' Module providing parallel IO routines for fld files '''
+""" Module providing parallel IO routines for fld files """
 
 import numpy as np
 
+
 def fld_file_read_vector_field(fh, byte_offset, ioh):
-    ''' Function used to read a vector field from a fld file'''
+    """Function used to read a vector field from a fld file"""
 
     # Associate
     fld_data_size = ioh.fld_data_size
@@ -17,38 +18,38 @@ def fld_file_read_vector_field(fh, byte_offset, ioh):
     tmp_dp_vector = ioh.tmp_dp_vector
 
     # Allocate
-    x = np.zeros(nelv*lxyz, dtype = np.double)
-    y = np.zeros(nelv*lxyz, dtype = np.double)
-    z = np.zeros(nelv*lxyz, dtype = np.double)
+    x = np.zeros(nelv * lxyz, dtype=np.double)
+    y = np.zeros(nelv * lxyz, dtype=np.double)
+    z = np.zeros(nelv * lxyz, dtype=np.double)
 
     # Read
     if fld_data_size == 4:
-        fh.Read_at_all(byte_offset, tmp_sp_vector, status = None)
+        fh.Read_at_all(byte_offset, tmp_sp_vector, status=None)
         i = 0
         for e in range(0, nelv):
             for j in range(0, lxyz):
-                x[e*lxyz+j] = tmp_sp_vector[i]
+                x[e * lxyz + j] = tmp_sp_vector[i]
                 i += 1
             for j in range(0, lxyz):
-                y[e*lxyz+j] = tmp_sp_vector[i]
+                y[e * lxyz + j] = tmp_sp_vector[i]
                 i += 1
             if gdim > 2:
                 for j in range(0, lxyz):
-                    z[e*lxyz+j] = tmp_sp_vector[i]
+                    z[e * lxyz + j] = tmp_sp_vector[i]
                     i += 1
     else:
-        fh.Read_at_all(byte_offset, tmp_dp_vector, status = None)
+        fh.Read_at_all(byte_offset, tmp_dp_vector, status=None)
         i = 0
         for e in range(0, nelv):
             for j in range(0, lxyz):
-                x[e*lxyz+j] = tmp_dp_vector[i]
+                x[e * lxyz + j] = tmp_dp_vector[i]
                 i += 1
             for j in range(0, lxyz):
-                y[e*lxyz+j] = tmp_dp_vector[i]
+                y[e * lxyz + j] = tmp_dp_vector[i]
                 i += 1
             if gdim > 2:
                 for j in range(0, lxyz):
-                    z[e*lxyz+j] = tmp_dp_vector[i]
+                    z[e * lxyz + j] = tmp_dp_vector[i]
                     i += 1
 
     # Reshape to pymech compatible
@@ -58,8 +59,9 @@ def fld_file_read_vector_field(fh, byte_offset, ioh):
 
     return x, y, z
 
+
 def fld_file_read_field(fh, byte_offset, ioh):
-    ''' Function used to read a scalar field from a fld file'''
+    """Function used to read a scalar field from a fld file"""
 
     # Associate
     fld_data_size = ioh.fld_data_size
@@ -72,22 +74,22 @@ def fld_file_read_field(fh, byte_offset, ioh):
     tmp_dp_field = ioh.tmp_dp_field
 
     # Allocate
-    x = np.zeros(nelv*lxyz, dtype = np.double)
+    x = np.zeros(nelv * lxyz, dtype=np.double)
 
     # Read
     if fld_data_size == 4:
-        fh.Read_at_all(byte_offset, tmp_sp_field, status = None)
+        fh.Read_at_all(byte_offset, tmp_sp_field, status=None)
         i = 0
         for e in range(0, nelv):
             for j in range(0, lxyz):
-                x[e*lxyz+j] = tmp_sp_field[i]
+                x[e * lxyz + j] = tmp_sp_field[i]
                 i += 1
     else:
-        fh.Read_at_all(byte_offset, tmp_dp_field, status = None)
+        fh.Read_at_all(byte_offset, tmp_dp_field, status=None)
         i = 0
         for e in range(0, nelv):
             for j in range(0, lxyz):
-                x[e*lxyz+j] = tmp_dp_field[i]
+                x[e * lxyz + j] = tmp_dp_field[i]
                 i += 1
 
     # Reshape to pymech compatible
@@ -97,7 +99,7 @@ def fld_file_read_field(fh, byte_offset, ioh):
 
 
 def fld_file_write_vector_field(fh, byte_offset, x, y, z, ioh):
-    ''' Function used to write a vector field to a fld file'''
+    """Function used to write a vector field to a fld file"""
 
     # Associate
     fld_data_size = ioh.fld_data_size
@@ -108,9 +110,9 @@ def fld_file_write_vector_field(fh, byte_offset, x, y, z, ioh):
     tmp_dp_vector = ioh.tmp_dp_vector
 
     # Reshape to be a column
-    x = x.reshape((nelv*lxyz))
-    y = y.reshape((nelv*lxyz))
-    z = z.reshape((nelv*lxyz))
+    x = x.reshape((nelv * lxyz))
+    y = y.reshape((nelv * lxyz))
+    z = z.reshape((nelv * lxyz))
 
     # Write
     if fld_data_size == 4:
@@ -118,38 +120,39 @@ def fld_file_write_vector_field(fh, byte_offset, x, y, z, ioh):
         i = 0
         for e in range(0, nelv):
             for j in range(0, lxyz):
-                tmp_sp_vector[i] = x[e*lxyz+j]
+                tmp_sp_vector[i] = x[e * lxyz + j]
                 i += 1
             for j in range(0, lxyz):
-                tmp_sp_vector[i] = y[e*lxyz+j]
+                tmp_sp_vector[i] = y[e * lxyz + j]
                 i += 1
             if gdim > 2:
                 for j in range(0, lxyz):
-                    tmp_sp_vector[i] = z[e*lxyz+j]
+                    tmp_sp_vector[i] = z[e * lxyz + j]
                     i += 1
 
-        fh.Write_at_all(byte_offset, tmp_sp_vector, status = None)
+        fh.Write_at_all(byte_offset, tmp_sp_vector, status=None)
 
     else:
         i = 0
         for e in range(0, nelv):
             for j in range(0, lxyz):
-                tmp_dp_vector[i] = x[e*lxyz+j]
+                tmp_dp_vector[i] = x[e * lxyz + j]
                 i += 1
             for j in range(0, lxyz):
-                tmp_dp_vector[i] = y[e*lxyz+j]
+                tmp_dp_vector[i] = y[e * lxyz + j]
                 i += 1
             if gdim > 2:
                 for j in range(0, lxyz):
-                    tmp_dp_vector[i] = z[e*lxyz+j]
+                    tmp_dp_vector[i] = z[e * lxyz + j]
                     i += 1
 
-        fh.Write_at_all(byte_offset, tmp_dp_vector, status = None)
+        fh.Write_at_all(byte_offset, tmp_dp_vector, status=None)
 
     return
 
+
 def fld_file_write_field(fh, byte_offset, x, ioh):
-    ''' Function used to write a scalar field to a fld file'''
+    """Function used to write a scalar field to a fld file"""
 
     # Associate
     fld_data_size = ioh.fld_data_size
@@ -159,7 +162,7 @@ def fld_file_write_field(fh, byte_offset, x, ioh):
     tmp_dp_field = ioh.tmp_dp_field
 
     # Reshape to single column
-    x = x.reshape((nelv*lxyz))
+    x = x.reshape((nelv * lxyz))
 
     # Write
     if fld_data_size == 4:
@@ -167,61 +170,63 @@ def fld_file_write_field(fh, byte_offset, x, ioh):
         i = 0
         for e in range(0, nelv):
             for j in range(0, lxyz):
-                tmp_sp_field[i] = x[e*lxyz+j]
+                tmp_sp_field[i] = x[e * lxyz + j]
                 i += 1
 
-        fh.Write_at_all(byte_offset, tmp_sp_field, status = None)
+        fh.Write_at_all(byte_offset, tmp_sp_field, status=None)
 
     else:
         i = 0
         for e in range(0, nelv):
             for j in range(0, lxyz):
-                tmp_dp_field[i] = x[e*lxyz+j]
+                tmp_dp_field[i] = x[e * lxyz + j]
                 i += 1
 
-        fh.Write_at_all(byte_offset, tmp_dp_field, status = None)
+        fh.Write_at_all(byte_offset, tmp_dp_field, status=None)
 
     return
 
+
 def fld_file_write_vector_metadata(fh, byte_offset, x, y, z, ioh):
-    ''' Function used to write metadata of a vector field to a fld file'''
+    """Function used to write metadata of a vector field to a fld file"""
 
     # Associate
     nelv = ioh.nelv
     gdim = ioh.gdim
 
-    buff = np.zeros(2*gdim*nelv, dtype = np.single)
+    buff = np.zeros(2 * gdim * nelv, dtype=np.single)
 
     j = 0
     for e in range(0, nelv):
-        buff[j+0] = np.min(x[e,:,:,:])
-        buff[j+1] = np.max(x[e,:,:,:])
-        buff[j+2] = np.min(y[e,:,:,:])
-        buff[j+3] = np.max(y[e,:,:,:])
+        buff[j + 0] = np.min(x[e, :, :, :])
+        buff[j + 1] = np.max(x[e, :, :, :])
+        buff[j + 2] = np.min(y[e, :, :, :])
+        buff[j + 3] = np.max(y[e, :, :, :])
         j += 4
         if gdim > 2:
-            buff[j+0] = np.min(z[e,:,:,:])
-            buff[j+1] = np.max(z[e,:,:,:])
+            buff[j + 0] = np.min(z[e, :, :, :])
+            buff[j + 1] = np.max(z[e, :, :, :])
             j += 2
 
-    fh.Write_at_all(byte_offset, buff, status = None)
+    fh.Write_at_all(byte_offset, buff, status=None)
 
     return
 
+
 def fld_file_write_metadata(fh, byte_offset, x, ioh):
-    ''' Function used to write metadata of a scalar field to a fld file'''
+    """Function used to write metadata of a scalar field to a fld file"""
 
     # Associate
     nelv = ioh.nelv
 
-    buff = np.zeros(2*nelv, dtype = np.single)
+    buff = np.zeros(2 * nelv, dtype=np.single)
 
     j = 0
     for e in range(0, nelv):
-        buff[j+0] = np.min(x[e,:,:,:])
-        buff[j+1] = np.max(x[e,:,:,:])
+        buff[j + 0] = np.min(x[e, :, :, :])
+        buff[j + 1] = np.max(x[e, :, :, :])
         j += 2
 
-    fh.Write_at_all(byte_offset, buff, status = None)
+    fh.Write_at_all(byte_offset, buff, status=None)
 
     return
