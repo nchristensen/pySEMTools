a713dfd2f88047589d40746af215fb29c2a66e5b
adperezm <adalberto0524@gmail.com>
2024-07-20 22:41:08 +0200

    All to all communication added to the router

--- examples/communication/data_transfer.py
+++ examples/communication/data_transfer.py
@@ -49,6 +49,46 @@ for j in range(len(sources)):
             if x == False:
                 print(f"Process failed in rank: {rank}")
 
+# ==========================================================================================
+# Example on communicating data between ranks, processing and recieving back using alltoallv
+# ==========================================================================================
+
+local_data = np.zeros(((rank+1)*10, 3), dtype=np.double)
+
+destination = [rank + 1, rank + 2]
+for i, dest in enumerate(destination):
+    if dest >= size:
+        destination[i] = dest - size
+
+# First send the data to the other ranks.
+# The data input in this case is not a list, therefore the same data is sent to all destinations 
+sources, recvbf = rt.all_to_all(destination = destination, data = local_data, dtype=np.double)
+
+#reshape the data
+for i in range(0, len(recvbf)):
+    recvbf[i] = recvbf[i].reshape((-1, 3))
+
+# Now modify the data
+for data in recvbf:
+    data[:] = rank
+
+# Now send the data back to the original rank
+# Now the destinations are the previous sources
+sources, recvbf = rt.all_to_all(destination = sources, data = recvbf, dtype=np.double)
+#reshape the data
+for i in range(0, len(recvbf)):
+    recvbf[i] = recvbf[i].reshape((-1, 3))
+
+# Test if the recieved data is what should have been
+testbuff = [local_data + destination[i] for i in range(len(destination))]
+
+for j in range(len(sources)):
+    for j in range(len(destination)):
+        if sources[i] == destination[j]:
+            x = (np.allclose(recvbf[i], testbuff[j]))
+            if x == False:
+                print(f"Process failed in rank: {rank}")
+
 # ==========================================================================
 # Example on gathering data from all ranks
 # ==========================================================================
