f74d3a2a40d51e0fdc480518f4d4170bbcf8cae4
Adalberto <adalberto0524@gmail.com>
2024-09-05 21:19:12 +0200

    Updated the examples

--- examples/statistics_processing/average_fields.py
+++ examples/statistics_processing/average_fields.py
@@ -5,5 +5,5 @@ from pynektools.postprocessing.statistics.time_averaging import average_field_fi
 comm = MPI.COMM_WORLD
 
 # Average the field files in the index
-dpath = "./sem_data/statistics/"
+dpath = "../data/sem_data/statistics/cylinder_rbc_nelv_600/"
 average_field_files(comm, field_index_name = dpath + "mean_field.fld_index.json", output_folder = dpath, output_batch_t_len=50)
