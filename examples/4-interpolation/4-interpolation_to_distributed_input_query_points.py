3dc1e84f38d3bb75f1a275da431dd146dda3cd24
Adalberto <adalberto0524@gmail.com>
2024-09-11 18:05:02 +0200

    Added a distributed example

--- examples/4-interpolation/4-interpolation_to_distributed_input_query_points.py
+++ examples/4-interpolation/4-interpolation_to_distributed_input_query_points.py
@@ -1,5 +1,4 @@
-# In this example, we interpolate the same points in every rank, so this should not be done in practice
-# A proper for this, would have different points in each rank
+# In this example, we provide a different set of points to each rank
 
 # Import required modules
 from mpi4py import MPI #equivalent to the use of MPI_init() in C
@@ -22,38 +21,63 @@ pynekread(fname, comm, data_dtype=np.double, msh=msh, fld=fld)
 import pynektools.interpolation.utils as interp_utils
 import pynektools.interpolation.pointclouds as pcs
 
-#if comm.Get_rank() == 0 :
-if 0 == 0:
-    # Generate the bounding box of the points
-    x_bbox = [0, 0.05]
-    y_bbox = [0, 2*np.pi]
-    z_bbox = [0 , 1]
-    nx = 30
-    ny = 30
-    nz = 30
-    
-    # Generate the 1D mesh
-    x_1d = pcs.generate_1d_arrays(x_bbox, nx, mode="equal")
-    y_1d = pcs.generate_1d_arrays(y_bbox, ny, mode="equal")
-    z_1d = pcs.generate_1d_arrays(z_bbox, nz, mode="equal")
+# Create the points in a distributed way in z direction
+# This will only really work for up to 8 ranks with these sizes
 
-    # Generate a 3D mesh
-    r, th, z = np.meshgrid(x_1d, y_1d, z_1d, indexing='ij')
-    x = r*np.cos(th)
-    y = r*np.sin(th)
+if comm.Get_size() > 8:
+    print("This example is only valid for 1, 2, 4, 8 ranks or less")
+    comm.Abort(1)
 
-    # Array the points as a list of probes
-    xyz = interp_utils.transform_from_array_to_list(nx,ny,nz,[x, y, z])
+x_bbox = [0, 0.05]
+y_bbox = [0, 2*np.pi]
+z_bbox = [0, 1]
+nx = 30
+ny = 30
+nz = 80
+nz_in_rank = int(nz/comm.Get_size())
 
-    # Write the points for future use
-    with open('points.csv', 'w') as f:
-        for i in range((xyz.shape[0])):
-            f.write(f"{xyz[i][0]},{xyz[i][1]},{xyz[i][2]}\n")
-else:
-    xyz = None
+# Generate the 1D mesh
+x_1d = pcs.generate_1d_arrays(x_bbox, nx, mode="equal")
+y_1d = pcs.generate_1d_arrays(y_bbox, ny, mode="equal")
+z_1d = pcs.generate_1d_arrays(z_bbox, nz, mode="equal")
+
+# Generate a 3D mesh
+r_, th_, z_ = np.meshgrid(x_1d, y_1d, z_1d, indexing='ij')
+#r_, th_, z_ = np.meshgrid(x_1d, y_1d, z_1d)
+x_ = r_*np.cos(th_)
+y_ = r_*np.sin(th_)
+
+r = np.copy(r_[:,:,nz_in_rank*comm.Get_rank():nz_in_rank*(comm.Get_rank()+1)])
+th = np.copy(th_[:,:,nz_in_rank*comm.Get_rank():nz_in_rank*(comm.Get_rank()+1)])
+x = np.copy(x_[:,:,nz_in_rank*comm.Get_rank():nz_in_rank*(comm.Get_rank()+1)])
+y = np.copy(y_[:,:,nz_in_rank*comm.Get_rank():nz_in_rank*(comm.Get_rank()+1)])
+z = np.copy(z_[:,:,nz_in_rank*comm.Get_rank():nz_in_rank*(comm.Get_rank()+1)])
+
+
+print(x.shape)
+
+# Array the points as a list of probes
+xyz = interp_utils.transform_from_array_to_list(nx,ny,nz_in_rank,[x, y, z])
 
 from pynektools.interpolation.probes import Probes
 
 probes = Probes(comm, probes = xyz, msh = msh, point_interpolator_type='multiple_point_legendre_numpy', max_pts=256, find_points_comm_pattern='point_to_point')
 
 probes.interpolate_from_field_list(0, [fld.registry['w']], comm, write_data=True)
+polar_fields = interp_utils.transform_from_list_to_array(nx,ny,nz_in_rank,probes.interpolated_fields)
+
+
+# Check the results individually per rank
+w = polar_fields[1]
+w_2d = np.mean(w, axis=1)
+levels = 500
+levels = np.linspace(-0.07, 0.07, levels)
+cmapp='RdBu_r'
+fig, ax = plt.subplots(1, 1,figsize=(5, 5))
+c1 = ax.tricontourf(r[:,0,:].flatten(), z[:,0,:].flatten() ,w_2d.flatten(), levels=levels, cmap=cmapp)
+fig.colorbar(c1)
+ax.set_xlabel("r")
+ax.set_ylabel("z")
+ax.set_xlim([0, 0.05])
+ax.set_ylim([0, 1])
+plt.show()
