3259f5a7e1ea7176c58b49e2cd0b302350cbf96f
Adalberto <adalberto0524@gmail.com>
2024-09-11 10:08:57 +0200

    Added more scripts to the stats example

---
+++ examples/statistics_processing/post_process_stat_files.py
@@ -0,0 +1,12 @@
+from mpi4py import MPI
+from pynektools.postprocessing.statistics.time_averaging import average_field_files
+from pynektools.postprocessing.file_indexing import index_files_from_folder
+
+# Get communicator
+comm = MPI.COMM_WORLD
+
+# Average the field files in the index
+dpath = "../data/sem_data/statistics/cylinder_rbc_nelv_600/"
+average_field_files(comm, field_index_name = dpath + "mean_field.fld_index.json", output_folder = dpath, output_batch_t_len=50)
+
+index_files_from_folder(comm, folder_path="../data/sem_data/statistics/cylinder_rbc_nelv_600/", run_start_time=0, stat_start_time = 50, include_time_interval=True, include_file_contents=True)
