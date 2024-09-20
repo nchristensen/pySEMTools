02adf778fc7ae460596b79fc89f1d771ffb0848a
Adalberto <adalberto0524@gmail.com>
2024-09-20 13:13:20 +0200

    Modified the typing of the inputs a bit

--- pynektools/rom/io_help.py
+++ pynektools/rom/io_help.py
@@ -19,7 +19,8 @@ class IoHelp:
         number_of_fields=1,
         batch_size=1,
         field_size=1,
-        data_type=np.double,
+        field_data_type=np.double,
+        mass_matrix_data_type=np.double,
         module_name="io_helper",
     ):
 
@@ -29,17 +30,17 @@ class IoHelp:
         self.nf = int(field_size * number_of_fields)
 
         # Allocate the buffers
-        self.xi = np.zeros((int(field_size * number_of_fields), 1), dtype=data_type)
+        self.xi = np.zeros((int(field_size * number_of_fields), 1), dtype=field_data_type)
         self.buff = np.zeros(
-            (int(field_size * number_of_fields), self.batch_size), dtype=data_type
+            (int(field_size * number_of_fields), self.batch_size), dtype=field_data_type
         )
 
         # Allocate the mass matrix
-        self.bm1 = np.zeros((int(field_size * number_of_fields), 1), dtype=data_type)
+        self.bm1 = np.zeros((int(field_size * number_of_fields), 1), dtype=mass_matrix_data_type)
 
         # Allocate the square root of the mass matrix
         self.bm1sqrt = np.zeros(
-            (int(field_size * number_of_fields), 1), dtype=data_type
+            (int(field_size * number_of_fields), 1), dtype=mass_matrix_data_type
         )
 
         # Instance object
