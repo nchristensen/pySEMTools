b81bed2ffcaf331f2a80af97550b84de506ecd03
Timofey Mukha <timofey.mukha@protonmail.com>
2025-03-09 11:00:27 +0300

    Apply black formatting

--- pysemtools/cli/extract_subdomain.py
+++ pysemtools/cli/extract_subdomain.py
@@ -7,27 +7,46 @@ from pysemtools.io.ppymech.neksuite import pynekread
 from pysemtools.datatypes.utils import write_fld_subdomain_from_list
 import argparse
 
+
 def parse_comma_separated(value):
     # Split the string by commas, strip spaces, and return a list
-    return [item.strip() for item in value.split(',')]
+    return [item.strip() for item in value.split(",")]
+
 
 def parse_bounds(value):
     # Split the string by commas, strip spaces from each part, and convert to float
-    return [float(x.strip()) for x in value.split(',')]
+    return [float(x.strip()) for x in value.split(",")]
+
 
 def main():
     comm = MPI.COMM_WORLD
-    parser = argparse.ArgumentParser(description="Extract a box-shaped subdomain from a field file")
+    parser = argparse.ArgumentParser(
+        description="Extract a box-shaped subdomain from a field file"
+    )
 
     # Define command-line arguments
-    parser.add_argument("--input_file", type=str, required=True,
-                        help="Path to the field file")
-    parser.add_argument("--output_file", type=str, required=True,
-                        help="Path to the output field file with the extracted subdomain")
-    parser.add_argument('--bounds', type=parse_bounds, required=True,
-                        help="Comma-separated list of 6 floats")
-    parser.add_argument('--fields', type=parse_comma_separated, default=None, required=False,
-                        help="Comma-separated list of field names to save in the extraction (optional, all fields by default)")
+    parser.add_argument(
+        "--input_file", type=str, required=True, help="Path to the field file"
+    )
+    parser.add_argument(
+        "--output_file",
+        type=str,
+        required=True,
+        help="Path to the output field file with the extracted subdomain",
+    )
+    parser.add_argument(
+        "--bounds",
+        type=parse_bounds,
+        required=True,
+        help="Comma-separated list of 6 floats",
+    )
+    parser.add_argument(
+        "--fields",
+        type=parse_comma_separated,
+        default=None,
+        required=False,
+        help="Comma-separated list of field names to save in the extraction (optional, all fields by default)",
+    )
 
     # Parse the arguments
     args = parser.parse_args()
@@ -45,5 +64,14 @@ def main():
         fields = list(fld.registry.keys())
 
     # Write the data in a subdomain
-    write_fld_subdomain_from_list(args.output_file, comm, mesh, field_list=[fld.registry[i] for i in fields],
-                                  subdomain=[[bounds[0], bounds[1]], [bounds[2], bounds[3]], [bounds[4], bounds[5]]])
+    write_fld_subdomain_from_list(
+        args.output_file,
+        comm,
+        mesh,
+        field_list=[fld.registry[i] for i in fields],
+        subdomain=[
+            [bounds[0], bounds[1]],
+            [bounds[2], bounds[3]],
+            [bounds[4], bounds[5]],
+        ],
+    )
