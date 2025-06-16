c75379eca80535f0c77f8873c9551b7295735576
Adalberto <adalberto0524@gmail.com>
2025-06-16 12:45:30 +0200

    Added mesh scaling

--- scripts/3-mesh_check/run.py
+++ scripts/3-mesh_check/run.py
@@ -1,3 +1,10 @@
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+
 # Import required modules
 from mpi4py import MPI #equivalent to the use of MPI_init() in C
 import matplotlib.pyplot as plt
@@ -123,7 +130,6 @@ def calculate_mean_dissipations():
     pynekread(sem_mesh_fname, comm, data_dtype=sem_dtype, msh=msh)
     coef = Coef(msh, comm)
 
-    uzt = np.zeros_like(msh.x, dtype=sem_dtype)
     eps_t = np.zeros_like(msh.x, dtype=sem_dtype)
     eps_k = np.zeros_like(msh.x, dtype=sem_dtype)
 
@@ -139,9 +145,6 @@ def calculate_mean_dissipations():
             v = fld.registry['v']
             w = fld.registry['w']
             t = fld.registry['t']
-
-            # Convective heat flux
-            uzt_ = w*t
 
             mu = np.sqrt(pr/ra)
             lamb = 1 / np.sqrt(ra*pr)
@@ -175,23 +178,90 @@ def calculate_mean_dissipations():
             # Read the field
             pynekread(fname, comm, data_dtype=sem_dtype, fld=fld)
 
-            uzt_ = fld.registry[dissipation_keys[0]]
-            eps_k_ = fld.registry[dissipation_keys[1]]
-            eps_t_ = fld.registry[dissipation_keys[2]]
+            eps_k_ = fld.registry[dissipation_keys[0]]
+            eps_t_ = fld.registry[dissipation_keys[1]]
 
         # Update
-        uzt += uzt_
         eps_k += eps_k_
         eps_t += eps_t_
         fld.clear()
 
     # Average
-    uzt /= len(file_sequence)
     eps_k /= len(file_sequence)
     eps_t /= len(file_sequence)
     
-    return uzt, eps_k, eps_t
+    return eps_k, eps_t
 
+def scale_mesh_kolmogorov(dx, dy, dz, eps_k, eps_t):
+
+    # Read the inputs file
+    with open('inputs.json', 'r') as f:
+        inputs = json.load(f)
+
+    sem_mesh_fname = inputs['spectral_element_mesh_fname']
+    sem_mesh_fname = "../../examples/data/sem_data/instantaneous/cylinder_rbc_nelv_600/field0.f00801"
+    sem_dtype_str = inputs.get('spectral_element_mesh_type_in_memory', 'single')
+    out_fname = inputs.get('msh_check_output_fname', 'msh_check0.f00001')
+    out_fname = f"kolmogorov_{out_fname}"
+    pr = inputs.get("pr")
+    ra = inputs.get("ra")
+    if sem_dtype_str == 'single':
+        sem_dtype = np.single
+    elif sem_dtype_str == 'double':
+        sem_dtype = np.double
+    else:
+        raise ValueError(f"Invalid spectral element mesh data type: {sem_dtype_str}")
+
+    # Read the mesh
+    msh = Mesh(comm)
+    pynekread(sem_mesh_fname, comm, data_dtype=sem_dtype, msh=msh)
+    coef = Coef(msh, comm)
+
+    # Get kolmogorov length scale
+    mu = np.sqrt(pr/ra)
+    lamb = 1 / np.sqrt(ra*pr)
+
+    eps_k_global_mean = coef.glsum(eps_k * coef.B, comm, dtype=sem_dtype) / coef.glsum(coef.B, comm, dtype=sem_dtype)
+    eps_t_global_mean = coef.glsum(eps_t * coef.B, comm, dtype=sem_dtype) / coef.glsum(coef.B, comm, dtype=sem_dtype)
+    eps_k_local_mean = np.sum(eps_k * coef.B, axis= (1,2,3)) / np.sum(coef.B, axis= (1,2,3))
+
+    eta_global = (mu**3 / eps_k_global_mean)**(1/4)
+    eta_local = (mu**3 / eps_k_local_mean)**(1/4)
+
+    dx_local = dx / eta_local.reshape(-1,1,1,1)
+    dy_local = dy / eta_local.reshape(-1,1,1,1)
+    dz_local = dz / eta_local.reshape(-1,1,1,1)
+    dx_local_avg = np.sum(dx_local*coef.B, axis = (1,2,3), keepdims=True) / np.sum(coef.B, axis = (1,2,3), keepdims=True) * np.ones_like(dx_local)
+    dy_local_avg = np.sum(dy_local*coef.B, axis = (1,2,3), keepdims=True) / np.sum(coef.B, axis = (1,2,3), keepdims=True) * np.ones_like(dy_local)
+    dz_local_avg = np.sum(dz_local*coef.B, axis = (1,2,3), keepdims=True) / np.sum(coef.B, axis = (1,2,3), keepdims=True) * np.ones_like(dz_local)
+
+    fld = FieldRegistry(comm)
+    fld.add_field(comm, field_name="dx_local", field=dx_local, dtype=sem_dtype)
+    fld.add_field(comm, field_name="dy_local", field=dy_local, dtype=sem_dtype)
+    fld.add_field(comm, field_name="dz_local", field=dz_local, dtype=sem_dtype)
+    fld.add_field(comm, field_name="dx_local_avg", field=dx_local_avg, dtype=sem_dtype)
+    fld.add_field(comm, field_name="dy_local_avg", field=dy_local_avg, dtype=sem_dtype)
+    fld.add_field(comm, field_name="dz_local_avg", field=dz_local_avg, dtype=sem_dtype)
+    pynekwrite(out_fname, comm, msh=msh, fld=fld)
+
+    # Write some data to verify
+    nu_eps_k = 1 + eps_k_global_mean/((mu**3)*ra/(pr**2))
+    nu_eps_t = 1 / lamb * eps_t_global_mean
+    max_dx_local_avg = comm.allreduce(np.max(dx_local_avg), op=MPI.MAX)
+    max_dy_local_avg = comm.allreduce(np.max(dy_local_avg), op=MPI.MAX)
+    max_dz_local_avg = comm.allreduce(np.max(dz_local_avg), op=MPI.MAX)
+
+    if comm.Get_rank() == 0:
+        with open("kolmogorov_data.json", 'w') as f:
+            data = {
+                "eta_global": eta_global,
+                "max_dx_kolmogorov": max_dx_local_avg,
+                "max_dy_kolmogorov": max_dy_local_avg,
+                "max_dz_kolmogorov": max_dz_local_avg,
+                "nu_eps_k": nu_eps_k,
+                "nu_eps_t": nu_eps_t
+            }
+            json.dump(data, f, indent=4)
 
 # ============
 # Main program
@@ -199,4 +269,6 @@ def calculate_mean_dissipations():
 
 dx, dy, dz = msh_spacing()
 
-uzt, eps_k, eps_t = calculate_mean_dissipations()
+eps_k, eps_t = calculate_mean_dissipations()
+
+scale_mesh_kolmogorov(dx, dy, dz, eps_k, eps_t)
